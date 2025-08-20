b0d37d2fc7fe465803baa18561fd870abdbc4db7
VarshUI <maverickvarsh@gmail.com>
2025-08-20 23:02:39 +0200

    user profiles displayed

--- SPA_userProfiles/src/App.jsx
+++ SPA_userProfiles/src/App.jsx
@@ -1,15 +1,19 @@
-import UserCard from './components/UserCard'
-import users from "./data/users.json"
-
+import UserCard from './components/UserCard';
+import users from "./data/users.json";
 
 const App = () => {
   return (
-    <div>
-      {users.map((user)=>( <UserCard key={user.id} user={user}/>))}
-    
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-4xl font-extrabold text-center mb-10 text-gray-800">
+        User Profiles
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- SPA_userProfiles/src/components/UserCard.jsx
+++ SPA_userProfiles/src/components/UserCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import emoji from "../data/emoji.json"
 const UserCard = ({user}) => {
   return (
-    <div> 
-        <p>Name: {user.name}</p>
-        <p>Age: {user.age}</p>
-        <p>Favourite Food: {user.favoriteFood}</p>
+    <div className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center text-center hover:shadow-xl hover:scale-105 transition-transform duration-300"> 
+         <img
+        src={user.profile_image}
+        alt={user.name}
+        className="w-24 h-24 rounded-full mb-4 border-2 border-gray-200"
+      />
+        <p className="text-xl font-semibold text-gray-800">Name: {user.name}</p>
+        <p  className="text-gray-500">Age: {user.age}</p>
+        <p  className="text-gray-500">Favourite Food: {user.favoriteFood} </p>
     </div>
   )
 }
